f99a9d61558ce1d166addf4809c08f85528af2b0
FernandoNoguera <fnoguerav25@gmail.com>
2023-07-20 11:22:09 -0400

    feat: add initial data

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+# Constants for movements and strikes
+MOVEMENTS = {"W", "S", "A", "D", "SD", "DSD", "SA", "ASA"}
+STRIKES = {"P", "K"}
+
+# Constants for character names
+ARNALDOR = "Arnaldor Shuatseneguer"
+TONYN = "Tonyn Stallone"
+
+# Dictionary to map character moves and strikes to their corresponding actions and energy usage
+CHARACTER_ACTIONS = {
+    "Tonyn Stallone": {
+        ("DSD", "P"): ("Taladoken", 3),
+        ("SD", "K"): ("Remuyuken", 2),
+        ("P", None): ("puñetazo", 1),
+        ("K", None): ("patada", 1),
+    },
+    "Arnaldor Shuatseneguer": {
+        ("SA", "K"): ("Remuyuken", 3),
+        ("ASA", "P"): ("Taladoken", 2),
+        ("P", None): ("puñetazo", 1),
+        ("K", None): ("patada", 1),
+    },
+}
+
+# Dictionary to map letter representations to words for movements and strikes
+MOVEMENTS_WORDS = {
+    "W": "arriba",
+    "S": "abajo",
+    "A": "izquierda",
+    "D": "derecha",
+    "SD": "diagonal inferior derecha",
+    "DSD": "diagonal superior derecha",
+    "SA": "diagonal superior izquierda",
+    "ASA": "diagonal inferior izquierda",
+}
+STRIKES_WORDS = {
+    "P": "puñetazo",
+    "K": "patada",
+}
+
+
 print("hello world!")
